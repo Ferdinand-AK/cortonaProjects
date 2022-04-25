265abde850a852b3dcccb55dc7d1d510d3b393f2
John Doe <johndoe@example.com>
2022-04-25 13:13:55 +0200

    init test project

---
+++ welcome.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+
+now = datetime.now()
+
+endOfWorkDay = now.replace(hour=17, minute=0, second=0, microsecond=0)
+endOfPraktikum = datetime(2022,4,29,17,0)
+
+todayWoringkHours = endOfWorkDay.hour - now.hour - 1
+todayWoringkMinutes = 60 - now.minute
+
+diffDays = endOfPraktikum.day - now.day 
+restDaysWorkingHours = diffDays * 8
+
+totalworkingHours = todayWoringkHours + restDaysWorkingHours
+strTotalworkingHours = str(totalworkingHours)
+
+print("Willkommen zum Praktikum Philipp.")
+print("Du hast noch " + strTotalworkingHours + " Stunden und " + str(todayWoringkMinutes) + " Minuten zu arbeiten.")
