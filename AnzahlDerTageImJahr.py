12aa5b9f1e21f3efa77584904a9222395c70802f
John Doe <johndoe@example.com>
2022-04-25 16:56:19 +0200

    Tag1

--- AnzahlDerTageImJahr.py
+++ AnzahlDerTageImJahr.py
@@ -1,1 +1,15 @@
-from datetime 
+from datetime import datetime
+print("Ich kann die Tage in einem beliebigen Jahr ausrechnen. Frage mich eine Zahl ab!")
+
+def getDaysOfYear(year):
+    startOfYear = datetime(year,1,1,0,0)
+    endOfYear= datetime(year+1,1,1,0,0)
+    return (endOfYear - startOfYear).days
+
+searchYear = input()
+searchYearNum = int(searchYear)
+
+print("Du hast mich das Jahr " + (searchYear) + " abgefragt. Hier ist deine Antwort.")
+tage = getDaysOfYear(searchYearNum)
+print(tage)
+print("Ich habs dir doch gesagt, dass ich die Tage in einem beliebigen Jahr ausrechnen kann.")
--- welcome.py
+++ welcome.py
@@ -5,7 +5,7 @@ now = datetime.now()
 endOfWorkDay = now.replace(hour=17, minute=0, second=0, microsecond=0)
 endOfPraktikum = datetime(2022,4,29,17,0)
 
-todayWoringkHours = endOfWorkDay.hour - now.hour - 1
+todayWoringkHours = endOfWorkDay.hour - now.hour -1
 todayWoringkMinutes = 60 - now.minute
 
 diffDays = endOfPraktikum.day - now.day 
